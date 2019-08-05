d5ce59163dbc81257f2e94747ae3b8a6a3411983
Somers Matthews <somersbmatthews@gmail.com>
2019-08-05 13:44:58 -0500

    created aggregator

---
+++ apps/aggregator/lib/instacook/aggregator.ex
@@ -0,0 +1,18 @@
+defmodule Instacook.Aggregator do
+  @moduledoc """
+  Documentation for Instacook.Aggregator.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Instacook.Aggregator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/aggregator/test/instacook/aggregator_test.exs
@@ -0,0 +1,8 @@
+defmodule Instacook.AggregatorTest do
+  use ExUnit.Case
+  doctest Instacook.Aggregator
+
+  test "greets the world" do
+    assert Instacook.Aggregator.hello() == :world
+  end
+end
